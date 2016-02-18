f00661de507dd23e2ad1518a1cdfbaecb3a18b44
yanshi <yanshi_hu@hiersun.com>
2016-02-18 17:20:35 +0800

    扫描二位码

--- CopyDome/CopyDome/ViewController.swift
+++ CopyDome/CopyDome/ViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import Foundation
+import AVFoundation
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var copyLabel: UILabel!
     
     @IBOutlet weak var stickTextField: UITextField!
     
+    @IBOutlet weak var inputNumTextField: UITextField!
+    
     @IBAction func copyAction(sender: AnyObject) {
-        var pboard = UIPasteboard.generalPasteboard()
+        let pboard = UIPasteboard.generalPasteboard()
         pboard.string = copyLabel.text
+    }
+    
+    @IBAction func inputAction(sender: AnyObject) {
+        //设置扫码区域参数
+        var style = LBXScanViewStyle()
+        style.centerUpOffset = 44;
+        style.photoframeAngleStyle = LBXScanViewPhotoframeAngleStyle.Inner;
+        style.photoframeLineW = 2;
+        style.photoframeAngleW = 18;
+        style.photoframeAngleH = 18;
+        style.isNeedShowRetangle = false;
+        
+        style.anmiationStyle = LBXScanViewAnimationStyle.LineMove;
+        
+        style.colorAngle = UIColor(red: 0.0/255, green: 200.0/255.0, blue: 20.0/255.0, alpha: 1.0)
+        
+        
+        style.animationImage = UIImage(named: "CodeScan.bundle/qrcode_Scan_weixin_Line")
+        
+        weak var weakSelf = self
+        let vc = LBXScanViewController();
+        vc.scanStyle = style
+        vc.resultString = {(result:LBXScanResult) in
+            weakSelf?.inputNumTextField.text = result.strScanned
+        }
+            
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
